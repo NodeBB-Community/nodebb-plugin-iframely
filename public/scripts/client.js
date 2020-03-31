c9061efb4cd1aefebc2294d0c140b36f5b5a8e0d
Nazar Leush <n.leush@gmail.com>
2020-03-31 20:32:09 +0300

    add listening to `action:posts.loaded` event to call iframely.load

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-    $(window).on('action:ajaxify.end action:composer.preview', function() {
+    $(window).on('action:ajaxify.end action:posts.loaded action:composer.preview', function() {
         /**
          * Iframely requires to call `iframely.load();` after widgets loaded to page.
-         * `action:ajaxify.end` - fires when posts rendered on page.
-         * `action:composer.preview` - fires when new preview rendered in composer.
+         * `action:ajaxify.end`         - triggered when posts rendered on page.
+         * `action:posts.loaded`        - triggered when you are on a topic page and a new post is sent to the client.
+         * `action:composer.preview`    - triggered when new preview rendered in composer.
          * In both cases Iframely need to initialize widgets.
          */
         iframely.load();
+    });
 
+    $(window).on('action:composer.preview', function() {
         /**
          * This logic prevents widget flickering while editing post in composer.
          * When user opens post editor, widget will be collapsed and `click to prevew` button will be shown.
